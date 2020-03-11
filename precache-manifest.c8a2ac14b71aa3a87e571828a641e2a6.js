faccfd6a739fa6d01ba15bb59bc4b49e0f998ab5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-11 03:36:41 +0000

    Deploy Charnnarong/digitconverter to github.com/Charnnarong/digitconverter.git:gh-pages

---
+++ precache-manifest.c8a2ac14b71aa3a87e571828a641e2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a7ff7c50103654c79f40be8db09c6d",
+    "url": "/digitconverter/index.html"
+  },
+  {
+    "revision": "5c2872d77dc8866a384e",
+    "url": "/digitconverter/static/css/main.8d52481c.chunk.css"
+  },
+  {
+    "revision": "b406a0df5112452c8551",
+    "url": "/digitconverter/static/js/2.b998fb05.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/digitconverter/static/js/2.b998fb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2872d77dc8866a384e",
+    "url": "/digitconverter/static/js/main.cf433f4f.chunk.js"
+  },
+  {
+    "revision": "466c42dcbc2f940a690e",
+    "url": "/digitconverter/static/js/runtime-main.f7b42ab4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/digitconverter/precache-manifest.3498f10c47fa7aa5ccf8f2465b354826.js"
+  "/digitconverter/precache-manifest.c8a2ac14b71aa3a87e571828a641e2a6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/digitconverter/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/digitconverter/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
